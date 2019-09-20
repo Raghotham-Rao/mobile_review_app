c807de2bd4cd597fc41204e722f5a4238eaa33c5
Raghotham Rao <mayberaghotham37@gmail.com>
2019-09-20 14:05:34 +0530

    initial contact section

--- ind-script.js
+++ ind-script.js
@@ -1,13 +1,26 @@
 var img1 = document.getElementById("img1").style;
 var img2 = document.getElementById("img2").style;
 
-var w1 = 100, w2 = 0;
+var w1 = 100, w2 = 0, test = 0;
 
 // var sldtimer = setInterval(timer, 50);
 
 function timer(){
-    w1 -= 10;
-    img1.width = w1 + "%";
-    w2 += 10;
-    img2.width = w2 + "%";
+    if(!test){
+        w1 -= 10;
+        img1.width = w1 + "%";
+        w2 += 10;
+        img2.width = w2 + "%";
+    }
+    else{
+        w1 += 10;
+        img1.width = w1 + "%";
+        w2 -= 10;
+        img2.width = w2 + "%";
+    }
+    if(w1 == 0 && w2 == 100){
+        test = 1;
+    }
+    else if(w2 == 0 && w1 == 100)
+        test = 0;
 }
